ef993bc37786b9ca7f7b800d06e35c600d2e059a
Ubuntu <ubuntu@ip-172-31-60-194.us-west-2.compute.internal>
2022-07-24 17:56:35 +0000

    Automated hand holding

--- run_exps.bash
+++ run_exps.bash
@@ -1,16 +1,34 @@
 #! /bin/bash
 
 export NUM_EXPS=$(expr $2 - $1)
-cd build
 mkdir -p dig_logs
 #docker compose build --build-arg "SIGN=$(date +%Y%m%d-%H%M%S)"
-# ./tmux-run-docker-part1.bash
+cd build
+./tmux-run-docker-part1.bash
+cd ..
+echo "Setting network conditions..."
+./set_network_conditions.bash
+cd build
+echo "Starting experiments"
 if [[ $3 != "JUST_RESULTS" ]]
 then
 	for i in $(seq $1 $(expr $2 - 1))
 	do
 		echo $i
 		./tmux-run-docker-part2.bash $i
+		export FILESIZE=$(wc -c dig_logs/run_$i.log)
+		while [[ $FILESIZE < 800 ]]
+		do
+			echo "Error with run $i"
+			docker compose down
+			echo "Resetting up docker env"
+			./tmux-run-docker-part1.bash
+			echo "Setting network conditions..."
+			./../set_network_conditions.bash
+			echo "Rerunning $i"
+			./tmux-run-docker-part2.bash $i
+			FILESIZE=$(wc -c dig_logs/run_$i.log)
+		done
 	done
 fi
 
