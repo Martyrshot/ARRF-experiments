ca86f0264cf318bcdebdb558698e422fbfc336e0
Ubuntu <ubuntu@ip-172-31-60-194.us-west-2.compute.internal>
2022-07-24 18:15:54 +0000

    More automation

--- build/tmux-run-docker-part1.bash
+++ build/tmux-run-docker-part1.bash
@@ -1,4 +1,10 @@
 #! /bin/bash
+tmux has-session -t docker-bind
+if [[ $? == 0 ]]
+then
+	tmux kill-session docker-bind
+fi
+
 tmux new-session -d -s 'docker-bind' -n 'bind' 'docker compose up ns1_root'
 sleep 4
 tmux split-window -t 'docker-bind:0' -h 'docker compose up resolver'
@@ -9,7 +15,7 @@ tmux split-window -t 'docker-bind:0.0' -v 'docker exec -it build-client1-1 /bin/
 sleep 4
 #tmux select-pane -t docker-bind:0.1
 tmux send-keys -t docker-bind:0.1 'dig test.goertzen' Enter
-sleep 1
+sleep 10
 tmux capture-pane -t docker-bind:0.1 -pS - > setup.log
 tmux send-keys -t docker-bind:0.1 -R Enter
 tmux clear-history -t docker-bind:0.1
@@ -24,20 +30,24 @@ do
 	tmux send-keys -t docker-bind:0.1 '^c'
 	#tmux select-pane -t 2
 	tmux send-keys -t docker-bind:0.2 '^c'
-	sleep 13
-	tmux kill-session -t docker-bind
+	sleep 11
+	tmux has-session -t docker-bind > /dev/null
+	if [[ $? == 0 ]]
+	then
+		tmux kill-session docker-bind
+	fi
 	tmux new-session -d -s 'docker-bind' -n 'bind' 'docker compose up ns1_root'
-	sleep 2
+	sleep 4
 	tmux split-window -t 'docker-bind:0' -h 'docker compose up resolver'
-	sleep 2
+	sleep 4
 	tmux split-window -t 'docker-bind:0' -v 'docker compose up ns1_goertzen host1 client1'
-	sleep 2
+	sleep 4
 	#tmux select-pane -t bind:0
 	tmux split-window -t 'docker-bind:0.0' -v 'docker exec -it build-client1-1 /bin/bash'
-	sleep 2
+	sleep 4
 	tmux select-pane -t docker-bind:0.1
 	tmux send-keys -t docker-bind:0.1 'dig test.goertzen' Enter
-	sleep 1
+	sleep 10
 	tmux capture-pane -t docker-bind:0.1 -pS - > setup.log
 	tmux send-keys -t docker-bind:0.1 -R Enter
 	tmux clear-history -t docker-bind:0.1
