e12263a1fbeeea925fb8cce087e78c60d927f550
Ubuntu <ubuntu@ip-172-31-60-194.us-west-2.compute.internal>
2022-10-10 19:17:09 +0000

    Added run_scenarios.bash to repo

--- run_scenarios.bash
+++ run_scenarios.bash
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-export OUTPUTDIR="$(pwd)/x86_res/no-daemon/10ms_50mbps"
-export UDPSIZES="stock"
-export ALGS="RSASHA256 ECDSA256"
+export OUTPUTDIR="$(pwd)/x86_res/sequential/100ms_50mbps"
+export UDPSIZES="1232"
+export ALGS="FALCON512"
 export BUILDDIR="$(pwd)/build"
 export WORKINGDIR="$(pwd)"
 mkdir -p $OUTPUTDIR
